4452f96a1d21f73320821e4621e414d883085305
Samathia <sa614@can.ac.uk>
2013-01-25 18:56:32 +0000

    Altered Engine.java to clarify dictionary of animals. Also added a few comments on implementation advice to same file.

--- Engine.java
+++ Engine.java
@@ -4,14 +4,16 @@ import java.util.Stack;
 import java.util.Map;
 
 public abstract class Engine {
-	
+
 	private Stack<Scene> sceneStack;
 	private PlayerStats stats;
 	private Map<NodeType, Node> nodes; // Maps an enumerated nodeType to the actual node object.
-	// animalDictionary: how will this be represented?
-	
+	private Map<Integer, Animal> animalDictionary // This will only ever be loaded, never saved,
+                                            //so we can do weird stuff with the animals
+                                            // at launch time, e.g. load sprites into them.
+
 	public PlayerStats getStats() { return stats; }
-	
+
 	public void update(); // Re-render the screen.
 	public SatNavUpdate fetchSatNavData(); // Get data from the sat-nav process.
 	public void initialise() { // Initial setup when app is started.
@@ -28,5 +30,6 @@ public abstract class Engine {
 							// it. Cleanup the removed scene!
 	private void populateAnimal(Animal animal, boolean isBlack);
 				//Add an animal to the Nodes in which it should appear.
-	
+				//Always get animals from the animalDictionary! Never serialise any animal data.
+
 }
