913cd1fa0762b0c47e84e273231b3e7d2943d6cf
CiaranDeasy <cfd27@cam.ac.uk>
2013-01-28 17:36:35 +0000

    Removed .java files from base directory.

--- Scene.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.cam.groupproj.racethewilds;
-
-public interface Scene {
-
-	public void render(); // Draw the scene to the screen.
-	public void update(); // Updates the scene on the screen.
-	public void close(); // Tell the scene to cleanup.
-
-}
