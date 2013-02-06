1af2b642453553254a0aa182c811da3a4a614ad6
TomMcCarthy <tmjm33@gmail.com>
2013-02-06 18:37:01 +0000

    Update project/src/uk/ac/cam/groupproj/racethewild/PlayerStats.java
    First draft of PlayerStats

--- project/src/uk/ac/cam/groupproj/racethewild/PlayerStats.java
+++ project/src/uk/ac/cam/groupproj/racethewild/PlayerStats.java
@@ -1,29 +1,78 @@
 package uk.ac.cam.groupproj.racethewild;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.List;
 
-public class PlayerStats implements Serializable {
+import android.content.Context;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6404719382973480586L;
-	int currentMovePoints;
-	int totalMovePoints;
-	int totalDistance;
-	List<Integer> foundAnimals;
-	List<Integer> greyAnimals;
-	String currentNode;
-	
-	public int getCurrentMovePoints(){return currentMovePoints;}
-	public int getTotalMovePoints(){return totalMovePoints;}
-	public int getTotalDistance(){return totalDistance;}
+	public class PlayerStats implements Serializable {
 
-	public String getCurrentNode(){return currentNode;}
-	
-	
-	//public static PlayerStats load(); // Loads savedata and gives back a stats object.
-	//public void save(); // Save stats to a file.
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 0L;
+		private int currentMovePoints;
+		private int totalMovePoints;
+		private int totalDistance;
+		private List<Integer> whiteAnimals;
+		private List<Integer> greyAnimals;
+		private Node currentNode;
+		private static String fileName = "PlayerStats";
+		
+		public PlayerStats(Node currentNode){
+			currentMovePoints = 0;
+			totalMovePoints = 0;
+			totalDistance = 0;
+			this.currentNode = currentNode;
+		}
+
+		public int getCurrentMovePoints(){return currentMovePoints;}
+		public int getTotalMovePoints(){return totalMovePoints;}
+		public int getTotalDistance(){return totalDistance;}
+		public List<Integer> getFoundAnimals(){return whiteAnimals;}
+		public List<Integer> getGreyAnimals(){return greyAnimals;}
+		public Node getCurrentNode(){return currentNode;}
+		
+		public void addMovePoints(int movePoints){
+			this.currentMovePoints =  this.currentMovePoints + movePoints;
+			this.totalMovePoints = this.totalMovePoints + movePoints;
+		}
+		
+		public void resetMovePoints(){
+			this.currentMovePoints = 0;
+		}
+		
+		public void addDistance(int distance){
+			this.totalDistance = this.totalDistance + distance;
+		}
+		
+		public void setCurrentNode(Node node){
+			this.currentNode = node;
+		}
+		
+		
+		//TODO: Add proper exception handling
+		// Loads savedata and gives back a stats object.
+		public static PlayerStats load(Context c) throws IOException, ClassNotFoundException{
+			FileInputStream fis = c.openFileInput(fileName);
+			ObjectInputStream is = new ObjectInputStream(fis);
+			PlayerStats stats = (PlayerStats) is.readObject();
+			is.close();
+			return stats;
+		}
+		
+		
+		public void save(Context c) throws IOException{
+			FileOutputStream fos;
+			fos =  c.openFileOutput(fileName, Context.MODE_PRIVATE);
+			ObjectOutputStream os = new ObjectOutputStream(fos);
+			os.writeObject(this);
+			os.close();
+		}
 
 }
