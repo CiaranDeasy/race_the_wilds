aeb30120d2aced43c9d1d30ab6344ddb51340d2b
Andrew Sheriff <as2197@cam.ac.uk>
2013-02-06 20:49:50 +0000

    more forgotten files

--- project/gen/uk/ac/cam/groupproj/racethewild/R.java
+++ project/gen/uk/ac/cam/groupproj/racethewild/R.java
@@ -23,15 +23,17 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_animal_collection=0x7f030000;
-        public static final int activity_main_menu=0x7f030001;
-        public static final int activity_node_scene=0x7f030002;
-        public static final int activity_scroll_map_scene=0x7f030003;
+        public static final int activity_check_in_scene=0x7f030001;
+        public static final int activity_main_menu=0x7f030002;
+        public static final int activity_node_scene=0x7f030003;
+        public static final int activity_scroll_map_scene=0x7f030004;
     }
     public static final class menu {
         public static final int activity_animal_collection=0x7f070000;
-        public static final int activity_main_menu=0x7f070001;
-        public static final int activity_node_scene=0x7f070002;
-        public static final int activity_scroll_map_scene=0x7f070003;
+        public static final int activity_check_in_scene=0x7f070001;
+        public static final int activity_main_menu=0x7f070002;
+        public static final int activity_node_scene=0x7f070003;
+        public static final int activity_scroll_map_scene=0x7f070004;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
@@ -42,6 +44,7 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_animal_collection=0x7f050006;
+        public static final int title_activity_check_in_scene=0x7f05000b;
         public static final int title_activity_node_scene=0x7f050009;
         public static final int title_activity_scroll_map_scene=0x7f050007;
         public static final int worldMap=0x7f050003;
