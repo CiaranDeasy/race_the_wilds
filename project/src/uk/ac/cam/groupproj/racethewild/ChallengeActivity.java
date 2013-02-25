eb0ec064527a33aca95f0e119b5f213ac9dc625e
AndrewSheriff <as2197@cam.ac.uk>
2013-02-25 22:09:42 +0000

    Adding support for distanceTime challenges

--- project/src/uk/ac/cam/groupproj/racethewild/ChallengeActivity.java
+++ project/src/uk/ac/cam/groupproj/racethewild/ChallengeActivity.java
@@ -15,17 +15,18 @@ public class ChallengeActivity extends Activity {
 	Challenge challenge = null;
 	TextView progressView;
 	String progress;
+	Engine e;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_challenge);
-		
+
 		Intent intent = getIntent();
 		String message = intent.getStringExtra(ChallengeList.CHALLENGE_NAME);
-		
-		Engine e = Engine.get();
-		
+
+		e = Engine.get();
+
 		List<Challenge> challenges = e.getAllChallenges();
 		int messageInt = Integer.parseInt(message);
 		for(Challenge c:challenges){
@@ -33,18 +34,18 @@ public class ChallengeActivity extends Activity {
 				challenge = c;
 			}
 		}
-		
+
 		message = challenge.getText();
-		
+
 		TextView challengeTitleTextView = (TextView) findViewById(R.id.challengeTitle);
 		challengeTitleTextView.setTextSize(20);
 		challengeTitleTextView.setText(message);
-		
+
 		progressView = (TextView) findViewById(R.id.challengeProgress);
 		progressView.setTextSize(40);
 		progressView.setText("Click to start challenge");
-		
-		
+
+
 		Thread runChallenge = new Thread(){
 			@Override
 			public void run(){
@@ -53,7 +54,7 @@ public class ChallengeActivity extends Activity {
 		};
 		runChallenge.start();
 	}
-	
+
 
 
 	@Override
@@ -62,24 +63,55 @@ public class ChallengeActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_challenge, menu);
 		return true;
 	}
-	
+
 	public Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message message){
 			progressView.setText(progress);
 		}
 	};
-	
+
 	public void runChallenge(Challenge challenge){
 		if(challenge==null){
+			return;
+		}
+		boolean tryChallenge = ChallengeController.requestMovementChallenge(this, challenge.getTime(), challenge.getRepetitions());
+		if(!tryChallenge){
 			return;
 		}
 		countDown(progressView);
 		//TODO implement challenges
 		progress = "Start Challenge";
 		handler.sendEmptyMessage(0);
+		if(challenge.getType()==ChallengeType.distanceTime){
+			ChallengeController.startMovementChallenge(this);
+			distanceChallengeStatus(challenge);
+		}
 	}
-	
+
+	public void distanceChallengeStatus(Challenge challenge){
+		ChallengeStatus status = ChallengeController.checkMovementChallengeStatus(this);
+		int distanceRemaining = challenge.getRepetitions() - status.distanceMoved;
+		long timeRemaining = (challenge.getTime()) - (status.timeTaken/1000);
+		while ((distanceRemaining > 0)&&(timeRemaining > 0)){
+			status = ChallengeController.checkMovementChallengeStatus(this);
+			distanceRemaining = challenge.getRepetitions() - status.distanceMoved;
+			timeRemaining = (challenge.getTime()) - (status.timeTaken/1000);
+			String distance = Integer.toString(distanceRemaining);
+			String time = Long.toString(timeRemaining);
+			progress = "Distance Remaining: " + distance + " Time Remaining: " + time;
+			handler.sendEmptyMessage(0);
+		}
+
+		if(distanceRemaining<0){
+			progress = "Challenge Passed!";
+			e.completeChallenge(challenge.getChallengeID(), this);
+		} else {
+			progress = "Challenge Failed";
+		}
+		handler.sendEmptyMessage(0);
+	}
+
 	public void countDown(TextView tview){
 		try {
 			for(int i=10;i>0;i--){
@@ -89,7 +121,7 @@ public class ChallengeActivity extends Activity {
 				Thread.sleep(1000);
 			}
 		} catch (InterruptedException e) {}
-		
+
 	}
 
 }
